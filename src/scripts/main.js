85b294985c4df72e850693d9165b8af8509ce941
BadOfficer <tarasbondarenko2005@gmail.com>
2025-08-25 19:37:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spyder = document.querySelector('.spider');
+
+const leftPos = wall.clientWidth / 2 - spyder.clientWidth / 2;
+const topPos = wall.clientHeight / 2 - spyder.clientHeight / 2;
+
+spyder.style.left = leftPos + 'px';
+spyder.style.top = topPos + 'px';
